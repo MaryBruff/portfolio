a5795afd39c3df82bc72ad62bd983666dafff249
Mary Bruff <marybruff5@gmail.com>
2025-08-13 08:25:32 -0600

    feat:add routes and folders for sections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="min-h-screen">
+      <body suppressHydrationWarning className="min-h-screen">
         {children}
         <Navigation />
       </body>
