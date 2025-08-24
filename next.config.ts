65a52c42a596068a044eac693b083adb968bb153
Mary Bruff <marybruff5@gmail.com>
2025-08-24 11:50:46 -0600

    Slow down the hot reload and delay

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpackDevMiddleware: (config: {
+      watchOptions: {
+        poll: number;
+        aggregateTimeout: number;
+      };
+    }) => {
+    config.watchOptions = {
+      poll: 1000, // Check for changes every second
+      aggregateTimeout: 300, // Delay rebuild after changes
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
