5cc0a583bbefa14d5273c49f1bee13e45ec40f8d
Mary Bruff <marybruff5@gmail.com>
2025-08-28 10:00:07 -0600

    Update next.config.js to support YouTube thumbnails and simplify Webpack config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  webpackDevMiddleware: (config: {
-      watchOptions: {
-        poll: number;
-        aggregateTimeout: number;
-      };
-    }) => {
-    config.watchOptions = {
-      poll: 1000, // Check for changes every second
-      aggregateTimeout: 300, // Delay rebuild after changes
-    };
-    return config;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        pathname: '/vi/**',
+      },
+    ],
   },
 };
 
