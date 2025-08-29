3bc4212127ac4264cc29e4d8ba302ba75ab28724
Mary Bruff <marybruff5@gmail.com>
2025-08-28 20:45:39 -0600

    Update button size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
+import Link from "next/link";
 import Window from '@/components/Window';
-import Button from '@/components/Button';
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 ">
       <Window title="Welcome to My Portfolio">
         <h1 className="text-2xl font-bold mb-4">Hello, I am [Mary]!</h1>
         <p className="mb-4">Explore my work in a retro Windows style.</p>
         <div className="flex space-x-4">
-          <Button href="/about">About Me</Button>
-          <Button href="/work">My Work</Button>
-          {/* <Button href="/contact">Contact</Button> */}
+          <Button variant="default" size="lg" asChild>
+            <Link href="/about">About Me</Link>
+          </Button>
+          <Button variant="default" size="lg" asChild>
+            <Link href="/work">My Work</Link>
+          </Button>
         </div>
       </Window>
+      <div className="p-4">
+    </div>
     </main>
   );
 }
