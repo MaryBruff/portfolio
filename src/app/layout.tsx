2eeab4e6ed39d7b4327a4776b75f3d5b7bff9b24
Mary Bruff <marybruff5@gmail.com>
2025-08-29 14:25:49 -0600

    Add the cube to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import Navigation from '@/components/Navigation';
+import TheCube from '@/components/framer/the_cube';
 
 export const metadata: Metadata = {
   title: "Mary Bruff Portfolio",
@@ -11,6 +12,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body suppressHydrationWarning className="min-h-screen">
+        <TheCube
+          scale={7.0}
+          speed={0.5}
+        />
         {children}
         <Navigation />
       </body>
