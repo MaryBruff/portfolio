ce8c59df6aa9513140bbd8c384facaf69a44c9f9
Mary Bruff <marybruff5@gmail.com>
2025-08-31 12:47:43 -0600

    Style scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning className="min-h-screen">
+      <body suppressHydrationWarning className="min-h-screen scrollbar">
         <TheCube
           scale={7.0}
           speed={0.25}
