67cd71e4f671de3189d51c39438487ce9082b220
Mary Bruff <marybruff5@gmail.com>
2025-09-01 13:43:07 -0600

    Still messing with the cube

--- src/components/framer/the_cube.tsx
+++ src/components/framer/the_cube.tsx
@@ -16,14 +16,14 @@ export default function TheCube({
     if (!ref.current) return;
 
     const rotate = Math.sin((t * speed) / 10000) * 200;
-    const y = (1 + Math.sin((t * speed) / 1000)) * 42 * scale;
+    const y = (1 + Math.sin((t * speed) / 1000)) * 1 * scale;
     ref.current.style.transform = `translateY(${y}px) rotateX(${rotate}deg) rotateY(${rotate}deg)`;
   });
 
   const baseSize = 200;
   const size = baseSize * scale;
   const translateZ = size / 2;
-  const perspective = baseSize * scale * 4; // Adjusted to scale more aggressively
+  const perspective = baseSize * scale * 1; // Adjusted to scale more aggressively
 
   function StyleSheet() {
     return (
