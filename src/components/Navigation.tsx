9b56856077757987807b8ad187a014d9dd1926a0
Mary Bruff <marybruff5@gmail.com>
2025-09-07 16:58:47 -0600

    Remove start button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,26 +5,21 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-background border-t border py-1 px-2 flex space-x-2 backdrop-blur-sm">
+    <nav className="fixed bottom-0 left-0 right-0 py-1 px-2 flex space-x-2 backdrop-blur-sm">
       <Button
-        variant="default"
-      >
-        Start
-      </Button>
-      <Button
-        variant="default"
+        variant="secondary"
         asChild
       >
         <Link href="/">Home</Link>
       </Button>
       <Button
-        variant="default"
+        variant="secondary"
         asChild
       >
         <Link href="/about">About</Link>
       </Button>
       <Button
-        variant="default"
+        variant="secondary"
         asChild
       >
         <Link href="/work">Work</Link>
