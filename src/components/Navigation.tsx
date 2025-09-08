3a9a3a889f356e3be1aadbf20e56bf1c7cdd3651
Mary Bruff <marybruff5@gmail.com>
2025-09-07 19:02:32 -0600

    Oops add bg back

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 py-1 px-2 flex space-x-2 backdrop-blur-sm">
+    <nav className="fixed bottom-0 left-0 right-0 bg-background border-t border py-1 px-2 flex space-x-2 backdrop-blur-sm">
       <Button
         variant="secondary"
         asChild
