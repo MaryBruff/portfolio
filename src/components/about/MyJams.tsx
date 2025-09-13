e9015f445878799f72ddaa7f4d2104337ec9a2a7
Mary Bruff <marybruff5@gmail.com>
2025-09-12 18:44:00 -0600

    Make Spotify iframe responsive

--- src/components/about/MyJams.tsx
+++ src/components/about/MyJams.tsx
@@ -2,12 +2,16 @@ import Window from "@/components/Window";
 
 export default function MyJams() {
   return (
-    <Window title="Currently Listenin' To">
-      <div className="flex items-center justify-between bg-accent border border-foreground p-2 rounded-lg">
-        <iframe
-          src="https://spotify-github-profile.kittinanx.com/api/view?uid=poptartmarbear&cover_image=true&theme=novatorem&show_offline=false&background_color=ff69b4&interchange=false&bar_color=ff64da"
-          height={100}
-        />
+    <Window title="Currently Listenin' To" className="max-w-3xl mx-auto">
+      <div className="bg-accent border border-foreground p-2 rounded-lg">
+        <div className="w-full max-w-xl mx-auto aspect-[350/100] overflow-hidden rounded-md">
+          <iframe
+            className="w-full h-full"
+            src="https://spotify-github-profile.kittinanx.com/api/view?uid=poptartmarbear&cover_image=true&theme=novatorem&show_offline=false&background_color=ff69b4&interchange=false&bar_color=ff64da"
+            title="Spotify Now Playing"
+            loading="lazy"
+          />
+        </div>
       </div>
     </Window>
   );
