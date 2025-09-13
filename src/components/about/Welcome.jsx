d30f6836c8f5295bc3d8e9903aa1297d4fdb7972
Mary Bruff <marybruff5@gmail.com>
2025-09-12 18:13:33 -0600

    Make dis responsive yo

--- src/components/about/Welcome.jsx
+++ src/components/about/Welcome.jsx
@@ -5,16 +5,14 @@ import Window from "@/components/Window";
 export default function Welcome() {
   return (
     <Window title="Welcome to My Portfolio!" className="max-w-3xl mx-auto">
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center w-full">
         <span
-          className="block w-[435px] h-[75px] bg-center bg-no-repeat"
+          className="block w-full max-w-md aspect-[435/75] bg-center bg-no-repeat bg-contain"
           style={{
             backgroundImage:
               "url('https://dl.glitter-graphics.com/pub/2670/2670560e3z842dbcp.gif')",
             visibility: "visible",
             backgroundColor: "transparent",
-            fontSize: "0px",
-            letterSpacing: "-0.5px",
           }}
         />
       </div>
