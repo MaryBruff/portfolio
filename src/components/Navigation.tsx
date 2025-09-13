13ba1e9b03472e4446f893eccb4ce887c69d43ca
Mary Bruff <marybruff5@gmail.com>
2025-09-12 21:40:42 -0600

    XP Navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,26 +1,37 @@
 "use client";
 import React from "react";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Navigation() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-background border-t border py-1 px-2 flex space-x-2 backdrop-blur-sm">
-      <Button
-        asChild
+    <nav aria-label="Primary" className="fixed bottom-0 left-0 right-0 z-[1000]">
+      <div 
+        className="justify-start rounded-none w-full items-center gap-2 px-[3px] py-[3px]"
+        style={{ 
+            background: `linear-gradient(
+              180deg,
+              rgba(9, 151, 255, 1) 0%,
+              rgba(0, 83, 238, 1) 8%,
+              rgba(0, 80, 238, 1) 40%,
+              rgba(0, 102, 255, 1) 88%,
+              rgba(0, 102, 255, 1) 93%,
+              rgba(0, 91, 255, 1) 95%,
+              rgba(0, 61, 215, 1) 96%,
+              rgba(0, 61, 215, 1) 100%
+            )`
+          }}
       >
-        <Link href="/">Home</Link>
-      </Button>
-      <Button
-        asChild
-      >
-        <Link href="/about">About</Link>
-      </Button>
-      <Button
-        asChild
-      >
-        <Link href="/work">Work</Link>
-      </Button>
+        <Button className="flex-none px-3 py-[2px]">
+          <Link href="/">Home</Link>
+        </Button>
+        <Button className="flex-none px-3 py-[2px]">
+          <Link href="/about">About</Link>
+        </Button>
+        <Button className="flex-none px-3 py-[2px]">
+          <Link href="/work">Work</Link>
+        </Button>
+      </div>
     </nav>
   );
 }
