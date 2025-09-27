e4200312c9bd561459a10cd9b7c5a6a6ef10abe5
Mary Bruff <marybruff5@gmail.com>
2025-09-27 14:40:16 -0600

    Prevent zoom on focus with input/textarea

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+      </head>
       <body suppressHydrationWarning className="min-h-screen bg-transparent">
         <div className="min-h-screen relative z-10">
           {children}
